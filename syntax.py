fcf79a3f1fc0ed0ffecd4d3eeac25caddce2582d
Fábio Melo <fabio-melo@users.noreply.github.com>
2018-08-25 22:49:17 -0300

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -1,86 +1,156 @@
-from utils import Token, alert
+from token import Token
+from alert import *
+
 
 class Syntax:
   def __init__(self, tokens, debug=True):
     self.tokens = tokens
     self._debug = ['Syntax', debug]
+    self._current_line = 0
 
-  def _get_symbol(self):
+
+  def _get(self):
     if self.tokens:
       symbol = self.tokens.pop(0)
+      self._current_line = symbol.line
       return symbol
     else:
       return Token(False,False,False)
   
 
-  def _read_symbol(self):
+  def _read(self):
     if self.tokens:
+      self._current_line = self.tokens[0].line
       return self.tokens[0]
     else:
       return Token(False,False,False)
 
 
-  def _alert(self, kind, alert):
-    if self._debug:
-      if kind == 0: print("INFO:  " + alert); return True
-      elif kind == 1: print("WARNING:" + alert); return True
-      elif kind == 2: print("ERROR: " + alert); return False
+
+  def _send_alert(self, code, alert_msg):
+    ERRORMSG = ' Line ' + str(self._current_line) + ':  ' + alert_msg
+    return alert(self._debug,code,ERRORMSG)
 
   def start(self):
     if not self.tokens:
       alert(self._debug, 2, "Cannot proceed: token list contains lexical errors.")
       return False
-
-    #alert(self._debug, 0, "------ PROGRAM BLOCK -----")
-    self._program() # program name
-    #alert(self._debug,0,"------ VAR BLOCK ---------")
-    self._variable_declarations() #var block
-    #alert(self._debug,0,"------ BEGIN BLOCK -------")
-    self._begin()
-    self._procedure()
+    self._simple_expression()
+    #self._program_id() # program name
+    #self._var_declarations() #var block
+     # self._subprograms()
+    #self._begin()
+    #self._procedure()
 
 
-  def _program(self):
-    if self._get_symbol().symbol == 'program' \
-    and self._get_symbol().kind == 'identifier' \
-    and self._get_symbol().symbol == ';':
-      return alert(self._debug,0,"Program Block Validated")  
-    else: return alert(self._debug,2,"undefined_program_declaration")
+  def _program_id(self):
+    if self._get().symbol == 'program':
+      if self._get().kind == 'identifier':
+        if self._get().symbol == ';':
+          return self._send_alert(0,P_DEFI_E + VALIDATED)  
+        else: return self._send_alert(2,P_DEFI_E + M_SEMI)
+      else: return self._send_alert(2,P_DEFI_E + M_IDENT)
+    else: return self._send_alert(2,P_DEFI_E + M_RESERV)
 
-  def _variable_declarations(self):
 
-    if self._read_symbol() and self._read_symbol().symbol == 'var':
-      self._get_symbol() # consumir o var
-      while self._read_symbol().kind is 'identifier':
-        self._identifier_declarations()
+  def _var_declarations(self):
+    if self._read().symbol == 'var':
+      self._get() # consumir o var
+      self._var_declaration_list()
+
+
+
+  def _var_declaration_list(self):
+    """ (Recursive)
+    < identifier_list > : <type> ;
+
+
+    """
+    inside = False
+    while True:
+      if self._read().kind == 'identifier':
+        inside = True
+        self._identifier_list()
+
+        if self._get().symbol == ':':
+          if self._get().symbol in DATA_TYPES:
+            if self._get().symbol == ';':
+              inside = False
+              self._send_alert(0, ID_DEF + VALIDATED)
+            else: return self._send_alert(2,ID_DEF + M_SEMI)
+          else: return self._send_alert(2,ID_DEF + M_TYPE)
+      elif not inside:
+        return self._send_alert(0, ID_DEF + CLEARED)
       else:
-        if self._read_symbol().symbol == 'begin':
-          return alert(self._debug,0,"var_block_cleared")
-        else:
-          return alert(self._debug,2,"broken_var_declaration_block")
+        return self._send_alert(2, ID_DEF + ERRORS)
 
-  def _begin(self):
-      if self._read_symbol() and self._read_symbol().symbol == 'begin':
-        self._procedure()
 
-      else: return alert(self._debug,2,'no_begin_block')
-      
-  def _procedure(self):
-    alert(self._debug,1,"NOT YET IMPLEMENTED")
-  
-  def _parameters(self):
-    pass
 
-  def _identifier_declarations(self):
-    DATA_TYPES = ['integer', 'real', 'boolean']
+  def _identifier_list(self):
+    """ (Recursive)
+    <identifier> | <identifier>,<identifier>
+    """
+    if self._read().kind == 'identifier':
+        self._get()
+        if self._read().symbol == ',':
+          self._get()
+          if self._read().kind == 'identifier':
+            self._identifier_list()
+          else: 
+            return self._send_alert(2, ID_DEF + M_IDENT)
 
-    if self._get_symbol().kind == 'identifier':
-      if self._get_symbol().symbol == ':':
-        if self._get_symbol().symbol in DATA_TYPES:
-          if self._get_symbol().symbol == ';':
-            return alert(self._debug,0, 'identifier_validated')
-          else: return alert(self._debug,2,'unclosed_var_declaration')
-        else: return alert(self._debug,2,'untyped_var_declaration')
-      else: return alert(self._debug,2,'missing_type_declaration')
-    else: return alert(self._debug,2,'broken_var_declaration_block')
-      
+  def _simple_expression(self):
+    """
+    <simple_ex> -> <termo> <simple_ex'> | <signal> <termo>
+    <simple_ex'> -> <addition> <termo> <simple_ex'>
+    """
+    # primeiro sinal (caso exista) e termo
+    if self._read().symbol in ['+','-']:
+      self._get(); self._term()
+    else: 
+      self._term()
+
+    # enquanto aparecer adição, loopar em busca de termos    
+    while self._read().kind == 'addition':
+      self._get(); self._term()
+
+
+  def _term(self):
+    """ 
+    <termo> -> <fator> <termo'> |
+    <termo'> -> <op_mult> <fator> <termo'>
+    | ϵ
+    """
+    self._factor()
+
+    if self._read().kind == 'multiplication': 
+      self._get(); self._term()
+    
+
+  def _factor(self):
+    """ (Factor)
+    """
+    factor = self._read()
+    if factor.kind in ['integer','real','boolean']:
+      self._get()
+      return self._send_alert(0, VALIDATED)
+    elif factor.kind == 'identifier': 
+      if self._read().symbol == '(':
+        pass
+        #TO-DO LIST OF EXPRESSION
+        if self._read().symbol == ')':
+          pass
+      else:
+        self._get() #VALIDADO IDENTIFICADOR
+        return self._send_alert(0, "FACTOR - " + VALIDATED)
+    elif factor.symbol == 'not':
+      self._factor()
+    elif factor.symbol == '(':
+      pass
+      # TO-DO EXPRESSION
+    else:
+      self._send_alert(2,"Missing Factor")
+    
+
+
+        
