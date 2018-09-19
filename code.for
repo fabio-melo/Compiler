ba836f23dba84318c1b3a356e8bb515be3b0660e
Fábio Melo <fabio-melo@users.noreply.github.com>
2018-09-19 11:33:10 -0300

    Add files via upload

--- code.for
+++ code.for
@@ -1,31 +1,13 @@
 program test; {programa exemplo}
-var
-  ii : integer;
-  rr : real;
-  bb: boolean;
 
 procedure outer(ii: real);
-  var
-    r2: real;
-    i2: integer;
+  var outer: real;
 
-  procedure inner;
-    var
-      b3: boolean;
-      r1,r2: real;
-
-    begin
-      r1 := 49 < 38;
-      b3 := true and false + true
-
-    end;
-  
   begin
-    r2 := ii + 5 ; {vai dar certo}
-    // i2 := r2 + i2; {vai dar errado}
-    r2 := i2 + 45.3 + 129 + r2
-  end
-  ;
+  end;
+procedure outer():
+  begin
+  end;
 
 begin
 end
